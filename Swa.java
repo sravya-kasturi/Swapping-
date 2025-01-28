0b2344ab37436aaf4bf1ed8a1504e193548035e4
KASTURI SRAVYA <kasturisravya15@gmail.com>
2025-01-28 16:03:16 +0530

    Add files via upload

---
+++ Swa.java
@@ -0,0 +1,14 @@
+public class Swa {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+    
+}
